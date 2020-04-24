3bcdb1498e2cc7340af76e7c504e961e7a53b413
lzplove1991 <869838811@qq.com>
2020-04-25 00:30:20 +0800

    1.ssd

--- SSD/SSD300.py
+++ SSD/SSD300.py
@@ -49,12 +49,38 @@ class SSD300:
             result = tf.nn.conv2d(input, shape, padding, strides, name = name + '_conv')
             result = tf.nn.bias_add(result, bias)
             if batch_normalization:
-                result = self.batch_normalization(name = name + '_bn')
+                result = self.batch_normalization(result, name = name + '_bn')
             result = tf.nn.relu(result, name = name + '_relu')
             return result
 
-    def batch_normalization(self, name):
-        pass
+    def batch_normalization(self, input, name):
+        with tf.variable_scope(name):
+            bn_input_shape = input.get_shape()
+            moving_mean = tf.get_variable(name + '_neam', bn_input_shape[-1:], initializer = tf.zeros_initializer, trainable = False)
+            moving_variance = tf.get_variable(name + '_variance', bn_input_shape[-1:], initializer = tf.ones_initializer, trainable = False)
+            def mean_var_with_update():
+                mean, variance = tf.nn.moments(input, list(range(len(bn_input_shape) - 1)), name = name + '_moments')
+                with tf.control_dependencies([assign_moving_average(moving_mean, mean, self.conv_bn_decay),
+                                              assign_moving_average(moving_variance, variance, self.conv_bn_decay)]):
+                    return tf.identity(mean), tf.identity(variance)
+            mean, variance = tf.cond(tf.case(True, tf.bool), mean_var_with_update, lambda : (moving_mean, moving_variance))
+            beta = tf.get_variable(name + '_beta', bn_input_shape[-1:],initializer = tf.zeros_initializer)
+            gamma = tf.get_variable(name + '_gamma', bn_input_shape[-1:], initializer = tf.ones_initializer)
+            return tf.nn.batch_normalization(input, mean, variance, beta, gamma, self.conv_bn_epsilon, name + '_bn_opt')
+
+    def smooth_L1(self, x):
+        return tf.where(tf.less_equal(tf.abs(x), 1.0), tf.multiply(0.5, tf.pow(x, 2.0)), tf.subtract(tf.abs(x), 0.5))
+
+    def fc(self, input, out_shape, name):
+        with tf.variable_scope(name + '_fc'):
+            in_shape = 1
+            for d in input.get_shape().as_list()[1:]:
+                in_shape += 1
+            weight = tf.get_variable(initializer = tf.truncated_normal([in_shape, out_shape], 0, 1), dtype = tf.float32, name = name + '_fc_weight')
+            bias = tf.get_variable(initializer = tf.truncated_normal([out_shape], 0, 1), dtype = tf.float32, name = name + '_fc_bias')
+            result = tf.reshape(input, [-1, in_shape])
+            result = tf.nn.xw_plus_b(result, weight, bias, name = name + '_fc_do')
+            return result
 
 
 
